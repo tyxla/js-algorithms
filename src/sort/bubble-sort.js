39a48ff8b0551e675bbbdd388a8d7005b4307ebf
Marin Atanasov <tyxla@abv.bg>
2015-02-03 23:11:25 +0200

    start hierarchy; sorting: bubblesort (initial)

---
+++ src/sort/bubble-sort.js
@@ -0,0 +1,28 @@
+'use strict';
+
+window.Algorithms = window.Algorithms || {};
+
+Algorithms.bubbleSort = function(arr, cmp) {
+
+	function comparator(a, b) {
+		return a - b;
+	}
+
+	function sort(arr, cmp) {
+		cmp = cmp || comparator;
+		var temp;
+
+		for (var i = 0; i < arr.length; i++) {
+			for (var j = i; j > 0; j--) {
+				if (cmp(arr[j], arr[j - 1]) < 0) {
+					temp = arr[j];
+					arr[j] = arr[j - 1];
+					arr[j - 1] = temp;
+				}
+			}
+		}
+		
+		return arr;
+	}
+
+};
