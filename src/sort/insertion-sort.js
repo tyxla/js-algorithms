8fb66d1cf45b56f193163175f8f0fe969253d52d
Marin Atanasov <tyxla@abv.bg>
2015-02-04 22:51:41 +0200

    Algorithms.Sort.insertionSort

---
+++ src/sort/insertion-sort.js
@@ -0,0 +1,42 @@
+// Using strict mode
+'use strict';
+
+// Define our globals
+window.Algorithms = window.Algorithms || {};
+Algorithms.Sort = Algorithms.Sort || {};
+
+/**
+ * Insertion sort algorithm.
+ * @param {array} arr Array of items to sort.
+ * @param {callback} cmp Optional callback to sort by.
+ */
+Algorithms.Sort.insertionSort = function(arr, cmp) {
+
+	// Default sort comparator
+	function comparator(a, b) {
+		return a > b;
+	}
+
+	// Process sorting
+	function sort(arr, cmp) {
+		cmp = cmp || comparator;
+		var len = arr.length, 
+			i = -1, 
+			j, 
+			tmp;
+
+		while (len--) {
+			tmp = arr[++i];
+			j = i;
+			while ( j-- && cmp(arr[j], tmp) ) {
+				arr[j + 1] = arr[j];
+			}
+			arr[j + 1] = tmp;
+		}
+
+		return arr;
+	}
+
+	return sort(arr, cmp);
+
+};
