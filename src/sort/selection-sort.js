82f6715ea3fc423a9ee957a6ab11bf480a733194
Marin Atanasov <tyxla@abv.bg>
2015-02-04 22:56:48 +0200

    Algorithms.Sort.selectionSort

---
+++ src/sort/selection-sort.js
@@ -0,0 +1,44 @@
+// Using strict mode
+'use strict';
+
+// Define our globals
+window.Algorithms = window.Algorithms || {};
+Algorithms.Sort = Algorithms.Sort || {};
+
+/**
+ * Selection sort algorithm.
+ * @param {array} arr Array of items to sort.
+ * @param {callback} cmp Optional callback to sort by.
+ */
+Algorithms.Sort.selectionSort = function(arr, cmp) {
+
+	// Default sort comparator
+	function comparator(a, b) {
+		return a > b;
+	}
+
+	// Process sorting
+	function sort(arr, cmp) {
+		cmp = cmp || comparator;
+		var i, j, tmp, tmp2;
+
+		for (i = 0; i < arr.length - 1; i++) {
+			tmp = i;
+			for (j = i + 1; j < arr.length; j++) {
+				if (arr[j] < arr[tmp]){
+					tmp = j;
+				}
+			}
+			if(tmp != i){
+				tmp2 = arr[tmp];
+				arr[tmp] = arr[i];
+				arr[i] = tmp2;
+			}
+		}
+
+		return arr;
+	}
+
+	return sort(arr, cmp);
+
+};
