c24bf2f57fb09735747db27775a704434478a706
Marin Atanasov <tyxla@abv.bg>
2015-02-04 22:24:11 +0200

    optimized bubble sort

--- src/sort/bubble-sort.js
+++ src/sort/bubble-sort.js
@@ -14,23 +14,25 @@ Algorithms.Sort.bubbleSort = function(arr, cmp) {
 
 	// Default sort comparator
 	function comparator(a, b) {
-		return a - b;
+		return a > b;
 	}
 
 	// Process sorting
 	function sort(arr, cmp) {
 		cmp = cmp || comparator;
-		var temp;
+		var swapped;
 
-		for (var i = 0; i < arr.length; i++) {
-			for (var j = i; j > 0; j--) {
-				if (cmp(arr[j], arr[j - 1]) < 0) {
-					temp = arr[j];
-					arr[j] = arr[j - 1];
-					arr[j - 1] = temp;
+		do {
+			swapped = false;
+			for (var i = 0; i < arr.length - 1; i++) {
+				if ( cmp(arr[i], arr[i + 1]) ) {
+					var temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+					swapped = true;
 				}
 			}
-		}
+		} while (swapped);
 
 		return arr;
 	}
