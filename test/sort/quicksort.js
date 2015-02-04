0f39e24dcca8940c44aa1a21cd36282af9c20459
Marin Atanasov <tyxla@abv.bg>
2015-02-04 23:12:06 +0200

    tests for Algorithms.Sort.quicksort

---
+++ test/sort/quicksort.js
@@ -0,0 +1,7 @@
+QUnit.test( "Quicksort", function( assert ) {
+	var original = [1, 10, 5, 3, 11, 9, 31, 4],
+		sorted = [1, 3, 4, 5, 9, 10, 11, 31],
+		result = Algorithms.Sort.quicksort(original);
+
+	assert.deepEqual( result, sorted, "Sorting Results" );
+});
