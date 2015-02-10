16a16415c3bf474a1fc140046b0cb4636e752f32
Marin Atanasov <tyxla@abv.bg>
2015-02-10 19:34:27 +0200

    tests for Algorithms.Sort.radixSort

---
+++ test/sort/radix-sort.js
@@ -0,0 +1,7 @@
+QUnit.test( "Radix Sort", function( assert ) {
+	var original = [1, 10, 5, 3, 11, 9, 31, 4],
+		sorted = [1, 3, 4, 5, 9, 10, 11, 31],
+		result = Algorithms.Sort.radixSort(original);
+
+	assert.deepEqual( result, sorted, "Sorting Results" );
+});
